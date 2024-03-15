c9f5152a65788e3fb60ac54266f4435b179b65ab
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-03-15 08:16:22 +0000

    second added solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
 
 const App = () => {
 
-  const {count,setCount} = useState(0);
+  const [count,setCount] = useState(0);
 
   const increment = () => {
     setCount(count+1);
